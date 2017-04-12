0df824fbd22e4eba3f0fac5d6fd0f3da8d5bf243
oscarcampbell <oscar.max.campbell@gmail.com>
2017-04-12 21:41:58 +0200

    Letzter commit nicht vollständig. Hier der Rest, der gefehlt hat.

--- abccccc/src/abccccc/main.java
+++ abccccc/src/abccccc/main.java
@@ -4,6 +4,7 @@ public class main {
 
 	public static void main(String[] args) {
 		// greeting
-		System.out.println("Hallo");
+		System.out.println("Hallo");#
+		System.out.println("Und Tschüß);
 	}
 }
